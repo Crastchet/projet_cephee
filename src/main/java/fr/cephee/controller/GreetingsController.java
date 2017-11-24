b0ff6394567c2f4b49318e422d900be3ab65b821
crastchet <crastchet@gmail.com>
2017-11-24 14:12:11 +0100

    ralcul

--- src/main/java/fr/cephee/controller/GreetingsController.java
+++ src/main/java/fr/cephee/controller/GreetingsController.java
@@ -1,4 +1,4 @@
-package controler;
+package fr.cephee.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
--- src/main/java/fr/cephee/controller/LoginController.java
+++ src/main/java/fr/cephee/controller/LoginController.java
@@ -1,4 +1,4 @@
-package controler;
+package fr.cephee.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
