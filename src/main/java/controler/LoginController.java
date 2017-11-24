cb826aa8b97fb138325b07a6163ccaa0cf4a16c4
Casier Sofian <casier@b12p9.fil.univ-lille1.fr>
2017-11-24 11:24:26 +0100

    refonte package

--- src/main/java/controler/LoginController.java
+++ src/main/java/controler/LoginController.java
@@ -1,5 +1,7 @@
-package fr.cephee.user;
+package controler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
@@ -9,8 +11,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import fr.cephee.helloworld.HelloWorldApplication;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Controller
 public class LoginController {
