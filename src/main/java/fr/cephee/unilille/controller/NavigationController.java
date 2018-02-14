f55e3cca4bb867bbf35890dfaafe76bc6f4298a1
neoragorn <neoragorn@github.com>
2018-02-14 15:04:54 +0100

    affichage pour moteur de recherche

--- src/main/java/fr/cephee/unilille/controller/NavigationController.java
+++ src/main/java/fr/cephee/unilille/controller/NavigationController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,14 +13,27 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import fr.cephee.unilille.database.CategoryPersistence;
+import fr.cephee.unilille.database.CompetencePersistence;
 import fr.cephee.unilille.database.MemberPersistence;
 import fr.cephee.unilille.database.PublicationPersistence;
+import fr.cephee.unilille.model.Category;
+import fr.cephee.unilille.model.Competence;
 import fr.cephee.unilille.model.Member;
 import fr.cephee.unilille.model.Publication;
+import fr.cephee.unilille.model.PublicationForm;
 
 @Controller
 public class NavigationController {
 
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	private CategoryPersistence dataCate;
+
+	@Autowired
+	private CompetencePersistence dataComp;
+	
 	@Autowired
 	private MemberPersistence datamem;
 	
@@ -39,8 +54,27 @@ public class NavigationController {
 	@RequestMapping(value = "/research")
 	public String goToResearch(Model model,
 			HttpSession session) {
+		List<Category> listcategory = dataCate.findAll();
+		List<Competence> listcompetence = dataComp.findAll();
+		log.info("ENTERED RESEARCH");
 		model.addAttribute("member", session.getAttribute("member"));
-		
+		//model.addAttribute("categoryList", listcategory);
+		//model.addAttribute("competenceList", listcompetence);
+		PublicationForm publicationForm = new PublicationForm();
+
+		publicationForm.setListCategory(listcategory);
+		publicationForm.setListCompetence(listcompetence);
+		model.addAttribute("publicationForm", publicationForm);
 		return "research";
+	}
+	
+	@RequestMapping(value = "/searching")
+	public String searching(Model model,
+			HttpSession session) {
+
+		
+		model.addAttribute("member", session.getAttribute("member"));
+
+		return "researchResult";
 	}
 }
