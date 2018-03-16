07ffc0ec6f13bc8a00050f217015a4b69a5e834c
neoragorn <neoragorn@github.com>
2018-03-16 11:50:11 +0100

    func pour avoir la liste evenement participe / a participe sur accueil

--- src/main/java/fr/cephee/unilille/controller/LoginController.java
+++ src/main/java/fr/cephee/unilille/controller/LoginController.java
@@ -1,6 +1,8 @@
 package fr.cephee.unilille.controller;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -19,12 +21,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import fr.cephee.unilille.database.CategoryPersistence;
 import fr.cephee.unilille.database.MemberInterestPersistence;
 import fr.cephee.unilille.database.MemberPersistence;
+import fr.cephee.unilille.database.ParticipantDataPersistence;
 import fr.cephee.unilille.database.PublicationPersistence;
 import fr.cephee.unilille.model.Category;
 import fr.cephee.unilille.model.Member;
-import fr.cephee.unilille.model.MemberForm;
 import fr.cephee.unilille.model.MemberInterest;
+import fr.cephee.unilille.model.Participantdata;
 import fr.cephee.unilille.model.Publication;
+import fr.cephee.unilille.model.PublicationEvent;
 
 
 @Controller
@@ -33,21 +37,24 @@ public class LoginController {
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
+	private ParticipantDataPersistence dataparticipant;
+
+	@Autowired
 	private MemberInterestPersistence datainterest;
 
 	@Autowired
 	private CategoryPersistence datacat;
-	
+
 	@Autowired
 	private MemberPersistence datamem;
 
 	@Autowired
 	Validator validator;
-	
+
 	@Autowired
 	private PublicationPersistence datapub;
-	
-	
+
+
 	@RequestMapping(value = {"/", "/home"})
 	public String returnToHome(Model model, HttpSession session, Authentication auth)
 	{	
@@ -63,9 +70,9 @@ public class LoginController {
 			session.setAttribute("member", member);
 		}
 		model.addAttribute("member", session.getAttribute("member"));
-		
-		
-	// !!!!!!!!!!!!!! PARTIE DE SOFIAN !!!!!!!!!!!!!!
+
+
+		// !!!!!!!!!!!!!! PARTIE DE SOFIAN !!!!!!!!!!!!!!
 		Member member = (Member) session.getAttribute("member");
 		if (datainterest.findByMember(member) == null)
 		{
@@ -77,16 +84,16 @@ public class LoginController {
 		List<Publication> tenLastPub = datapub.findTop10ByOrderByDateCreationDescByAuthorisedTrue(member.getId());
 		model.addAttribute("listlasttenpub", tenLastPub);
 
-		
+
 		List<List<Object>> lpubtotal = new ArrayList<>();
-		
+
 		MemberInterest memInt = datainterest.findByMember(member);
 
-		
+
 		for (Map.Entry<Category, Integer> entry : memInt.getInterests().entrySet())
 			if (entry.getValue() > 0)
-			lpubtotal.add(datapub.findTop20FiltredPublicationByCategoryandDateCreation(member.getId(), entry.getKey().getId()));
-		
+				lpubtotal.add(datapub.findTop20FiltredPublicationByCategoryandDateCreation(member.getId(), entry.getKey().getId()));
+
 		List<Publication> pubfiltred = new ArrayList<Publication>();
 		for (List<Object> lpub : lpubtotal)
 		{
@@ -99,12 +106,12 @@ public class LoginController {
 					{
 						Publication p = (Publication) ob;
 						if (!pubfiltred.contains(p))
-						pubfiltred.add(p);
+							pubfiltred.add(p);
 					}
 				}
 			}
 		}
-		
+
 		ArrayList<Publication> finalFiltredPub = new ArrayList<Publication>();
 		for (Map.Entry<Category, Integer> entry : memInt.getInterests().entrySet())
 		{
@@ -112,39 +119,55 @@ public class LoginController {
 			for (Publication p : pubfiltred)
 			{
 				for (Category c : p.getCategory())
-				if (memInt.getInterests().containsKey(c))
-				{
-					if (!finalFiltredPub.contains(p))
-						finalFiltredPub.add(p);
-					i++;
-				}
+					if (memInt.getInterests().containsKey(c))
+					{
+						if (!finalFiltredPub.contains(p))
+							finalFiltredPub.add(p);
+						i++;
+					}
 				if (i == entry.getValue())
 					break;
 			}
 		}
-//		for (Map.Entry<Category, Integer> entry : memInt.getInterests().entrySet())
-//		{
-//			for (int i = 0; i < entry.getValue(); i++)
-//			{
-// 
-//			}
-//		}
-		
+
 		for (Publication p : finalFiltredPub)
 		{
 			log.info("p  : " + p.getTitle());
-				for (Category c : p.getCategory())
-				{
-					log.info("cat = " + c.getTitle());
-				}
+			for (Category c : p.getCategory())
+			{
+				log.info("cat = " + c.getTitle());
+			}
 		}
-		
+
+
+		//partie pour les evenements auquels on participe
+		List<Publication> publiuser = member.getListpublication();
+		List<PublicationEvent> publiUserParticipate = new ArrayList<>();
+		List<PublicationEvent> publiUserHasParticipated = new ArrayList<>();
+		Date d = Calendar.getInstance().getTime();
+		for (Publication p : publiuser)
+		{
+			if (p instanceof PublicationEvent)
+			{
+				Participantdata data = dataparticipant.findByMemByPubli(member.getId(), p.getId());			
+				if (data != null)
+					if (((PublicationEvent) p).getStartevent().after(d))
+					{
+						publiUserParticipate.add((PublicationEvent) p);	
+					}
+					else
+						publiUserHasParticipated.add((PublicationEvent) p);	
+			}
+		}
+
+		model.addAttribute("listparticipateuser", publiUserParticipate);
+		model.addAttribute("listhasparticipateduser", publiUserHasParticipated);
 		model.addAttribute("listlasttenpub", tenLastPub);
 		model.addAttribute("finalFiltredPub", pubfiltred);
-		
+
 		return "home";
 	}
-	
+
 	/*
 	@RequestMapping(value = "/logout")
 	public String disconnect(Model model, HttpSession session)
@@ -154,80 +177,80 @@ public class LoginController {
 		model.addAttribute("memberForm", memberForm);
 		return "home";
 	}
-	*/
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-//	@RequestMapping(value = {"/", "/login"})
-//	public String loginPage(Model model)
-//	{		
-//		MemberForm memberForm = new MemberForm();
-//		model.addAttribute("memberForm", memberForm);
-//		return "login";
-//	}
-	
-//	@RequestMapping("/createmember")
-//	@ResponseBody
-//	public String create(@RequestParam(value="login", required=true) String login,
-//			@RequestParam(value="firstname", required=true) String firstname,
-//			@RequestParam(value="lastname", required=true) String lastname)
-//	{
-//		try {
-//			Member member = new Member();
-//			member.setLogin(login);
-//			member.setFirstname(firstname);
-//			member.setLastname(lastname);
-//			datamem.save(member);
-//		}
-//		catch (Exception ex) {
-//			return "Error creating the member: " + ex.toString();
-//		}
-//		return "member succesfully created with login = " + login;
-//	}
+	 */
 
-//	@RequestMapping("/createmembertest")
-//	@ResponseBody
-//	public String createTest(@RequestParam(value="login", required=true) String login)
-//	{
-//		if (datamem.findByLogin(login) != null)
-//			return "Error creating the member: " + login + " alreay existing";
-//		try {
-//			Member member = new Member();
-//			member.setLogin(login);
-//			member.setFirstname("test1");
-//			member.setLastname("test2");
-//			member.setListpublication(null);
-//			member.setSkills(null);
-//			datamem.save(member);
-//		}
-//		catch (Exception ex) {
-//			return "Error creating the member: " + ex.toString();
-//		}
-//		return "Membre créee avec le login :  " + login + " Retournez sur la page précédente et connectez vous ";
-//	}
-	
-//	@RequestMapping("/deletemember")
-//	@ResponseBody
-//	public String delete(@RequestParam(value="id", required=true) int uid) {
-//		try {
-//			datamem.delete(uid);
-//		}
-//		catch (Exception ex) {
-//			return "Error deleting the member:" + ex.toString();
-//		}
-//		return "member succesfully deleted!";
-//	}
 
-	
-	
-/*	@RequestMapping(value = "/getmemberbylogin", method = RequestMethod.POST)
+
+
+
+
+
+
+
+
+	//	@RequestMapping(value = {"/", "/login"})
+	//	public String loginPage(Model model)
+	//	{		
+	//		MemberForm memberForm = new MemberForm();
+	//		model.addAttribute("memberForm", memberForm);
+	//		return "login";
+	//	}
+
+	//	@RequestMapping("/createmember")
+	//	@ResponseBody
+	//	public String create(@RequestParam(value="login", required=true) String login,
+	//			@RequestParam(value="firstname", required=true) String firstname,
+	//			@RequestParam(value="lastname", required=true) String lastname)
+	//	{
+	//		try {
+	//			Member member = new Member();
+	//			member.setLogin(login);
+	//			member.setFirstname(firstname);
+	//			member.setLastname(lastname);
+	//			datamem.save(member);
+	//		}
+	//		catch (Exception ex) {
+	//			return "Error creating the member: " + ex.toString();
+	//		}
+	//		return "member succesfully created with login = " + login;
+	//	}
+
+	//	@RequestMapping("/createmembertest")
+	//	@ResponseBody
+	//	public String createTest(@RequestParam(value="login", required=true) String login)
+	//	{
+	//		if (datamem.findByLogin(login) != null)
+	//			return "Error creating the member: " + login + " alreay existing";
+	//		try {
+	//			Member member = new Member();
+	//			member.setLogin(login);
+	//			member.setFirstname("test1");
+	//			member.setLastname("test2");
+	//			member.setListpublication(null);
+	//			member.setSkills(null);
+	//			datamem.save(member);
+	//		}
+	//		catch (Exception ex) {
+	//			return "Error creating the member: " + ex.toString();
+	//		}
+	//		return "Membre créee avec le login :  " + login + " Retournez sur la page précédente et connectez vous ";
+	//	}
+
+	//	@RequestMapping("/deletemember")
+	//	@ResponseBody
+	//	public String delete(@RequestParam(value="id", required=true) int uid) {
+	//		try {
+	//			datamem.delete(uid);
+	//		}
+	//		catch (Exception ex) {
+	//			return "Error deleting the member:" + ex.toString();
+	//		}
+	//		return "member succesfully deleted!";
+	//	}
+
+
+
+	/*	@RequestMapping(value = "/getmemberbylogin", method = RequestMethod.POST)
 	public String getByLogin(@ModelAttribute("memberForm") MemberForm memberForm,
 			BindingResult result,
 			Model model,
@@ -248,19 +271,19 @@ public class LoginController {
 		}
 		model.addAttribute("member", member);
 		session.setAttribute("member", member);
-				
+
 		List<Publication> tenLastPub = datapub.findTop10ByOrderByDateCreationDescByAuthorisedTrue(member.getId());
 
 		//Debut du système publication par filtre
 
 		List<List<Object>> lpubtotal = new ArrayList<>();
-		
+
 		MemberInterest memInt = datainterest.findByMember(member);
 
 		for (Map.Entry<Category, Integer> entry : memInt.getInterests().entrySet())
 			if (entry.getValue() > 0)
 			lpubtotal.add(datapub.findTop20FiltredPublicationByCategoryandDateCreation(member.getId(), entry.getKey().getId()));
-		
+
 		List<Publication> pubfiltred = new ArrayList<Publication>();
 		for (List<Object> lpub : lpubtotal)
 		{
@@ -278,7 +301,7 @@ public class LoginController {
 				}
 			}
 	}
-		
+
 		ArrayList<Publication> finalFiltredPub = new ArrayList<Publication>();
 	for (Map.Entry<Category, Integer> entry : memInt.getInterests().entrySet())
 		{
@@ -300,10 +323,10 @@ public class LoginController {
 		{
 			for (int i = 0; i < entry.getValue(); i++)
 			{
- 
+
 			}
 		}
-		
+
 		for (Publication p : finalFiltredPub)
 	{
 			log.info("p  : " + p.getTitle());
@@ -312,29 +335,29 @@ public class LoginController {
 					log.info("cat = " + c.getTitle());
 				}
 		}
-		
+
 	model.addAttribute("listlasttenpub", tenLastPub);
 	model.addAttribute("finalFiltredPub", pubfiltred);
 	return "home";
 	}*/
 
-//	@RequestMapping("/updatemember")
-//	@ResponseBody
-//	public String updateUser(@RequestParam(value="id", required=true) int id,
-//			@RequestParam(value="email", required=true) String email,
-//			@RequestParam(value="firstname", required=true) String firstname,
-//			@RequestParam(value="lastname", required=true) String lastname) {
-//		try {
-//			Member user = datamem.findOne(id);
-//			user.setEmail(email);
-//			user.setFirstname(firstname);
-//			user.setLastname(lastname);
-//			datamem.save(user);
-//		}
-//		catch (Exception ex) {
-//			return "Error updating the member: " + ex.toString();
-//		}
-//		return "member succesfully updated!";
-//	}
+	//	@RequestMapping("/updatemember")
+	//	@ResponseBody
+	//	public String updateUser(@RequestParam(value="id", required=true) int id,
+	//			@RequestParam(value="email", required=true) String email,
+	//			@RequestParam(value="firstname", required=true) String firstname,
+	//			@RequestParam(value="lastname", required=true) String lastname) {
+	//		try {
+	//			Member user = datamem.findOne(id);
+	//			user.setEmail(email);
+	//			user.setFirstname(firstname);
+	//			user.setLastname(lastname);
+	//			datamem.save(user);
+	//		}
+	//		catch (Exception ex) {
+	//			return "Error updating the member: " + ex.toString();
+	//		}
+	//		return "member succesfully updated!";
+	//	}
 
 }
