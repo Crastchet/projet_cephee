6aaf5aa2283170fda6bd227580e3c6bbe85f0427
Thibs <crastchet@gmail.com>
2018-03-29 16:46:57 +0200

    logout fonctionne !

--- src/main/java/fr/cephee/unilille/controller/LoginController.java
+++ src/main/java/fr/cephee/unilille/controller/LoginController.java
@@ -9,6 +9,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.validation.Validator;
 
@@ -187,198 +188,14 @@ public class LoginController {
 
 		return "home";
 	}
+	
 
- 
-	/*
-	@RequestMapping(value = "/logout")
-	public String disconnect(Model model, HttpSession session)
-	{	
+	@RequestMapping("/logout")
+	public String logout(Model model, HttpSession session, Authentication auth) {
 		session.removeAttribute("member");
-		MemberForm memberForm = new MemberForm();
-		model.addAttribute("memberForm", memberForm);
-		return "home";
+		session.setMaxInactiveInterval(1);
+		auth.setAuthenticated(false);
+		return "logout";
 	}
-	 */
-
-
-
-
-
-
-
-
-
-
-	//	@RequestMapping(value = {"/", "/login"})
-	//	public String loginPage(Model model)
-	//	{		
-	//		MemberForm memberForm = new MemberForm();
-	//		model.addAttribute("memberForm", memberForm);
-	//		return "login";
-	//	}
-
-	//	@RequestMapping("/createmember")
-	//	@ResponseBody
-	//	public String create(@RequestParam(value="login", required=true) String login,
-	//			@RequestParam(value="firstname", required=true) String firstname,
-	//			@RequestParam(value="lastname", required=true) String lastname)
-	//	{
-	//		try {
-	//			Member member = new Member();
-	//			member.setLogin(login);
-	//			member.setFirstname(firstname);
-	//			member.setLastname(lastname);
-	//			datamem.save(member);
-	//		}
-	//		catch (Exception ex) {
-	//			return "Error creating the member: " + ex.toString();
-	//		}
-	//		return "member succesfully created with login = " + login;
-	//	}
-
-	//	@RequestMapping("/createmembertest")
-	//	@ResponseBody
-	//	public String createTest(@RequestParam(value="login", required=true) String login)
-	//	{
-	//		if (datamem.findByLogin(login) != null)
-	//			return "Error creating the member: " + login + " alreay existing";
-	//		try {
-	//			Member member = new Member();
-	//			member.setLogin(login);
-	//			member.setFirstname("test1");
-	//			member.setLastname("test2");
-	//			member.setListpublication(null);
-	//			member.setSkills(null);
-	//			datamem.save(member);
-	//		}
-	//		catch (Exception ex) {
-	//			return "Error creating the member: " + ex.toString();
-	//		}
-	//		return "Membre créee avec le login :  " + login + " Retournez sur la page précédente et connectez vous ";
-	//	}
-
-	//	@RequestMapping("/deletemember")
-	//	@ResponseBody
-	//	public String delete(@RequestParam(value="id", required=true) int uid) {
-	//		try {
-	//			datamem.delete(uid);
-	//		}
-	//		catch (Exception ex) {
-	//			return "Error deleting the member:" + ex.toString();
-	//		}
-	//		return "member succesfully deleted!";
-	//	}
-
-
-
-	/*	@RequestMapping(value = "/getmemberbylogin", method = RequestMethod.POST)
-	public String getByLogin(@ModelAttribute("memberForm") MemberForm memberForm,
-			BindingResult result,
-			Model model,
-			Errors errors,
-			HttpSession session) {
-
-		List<Category> cat = datacat.findAll();
-		Member member = datamem.findByUserName(memberForm.getUserName());
-		memberForm.setMember(member);
-		ValidationUtils.invokeValidator((org.springframework.validation.Validator) validator, memberForm, errors);		
-		if (result.hasErrors())	{
-			return "login";
-		}
-		if (datainterest.findByMember(member) == null)
-		{
-			MemberInterest meminterest = new MemberInterest(cat, member);
-			datainterest.save(meminterest);
-		}
-		model.addAttribute("member", member);
-		session.setAttribute("member", member);
-
-		List<Publication> tenLastPub = datapub.findTop10ByOrderByDateCreationDescByAuthorisedTrue(member.getId());
-
-		//Debut du système publication par filtre
-
-		List<List<Object>> lpubtotal = new ArrayList<>();
-
-		MemberInterest memInt = datainterest.findByMember(member);
-
-		for (Map.Entry<Category, Integer> entry : memInt.getInterests().entrySet())
-			if (entry.getValue() > 0)
-			lpubtotal.add(datapub.findTop20FiltredPublicationByCategoryandDateCreation(member.getId(), entry.getKey().getId()));
-
-		List<Publication> pubfiltred = new ArrayList<Publication>();
-		for (List<Object> lpub : lpubtotal)
-		{
-			Iterator<Object> itr = lpub.iterator();
-			while(itr.hasNext()){
-				Object[] obj = (Object[]) itr.next();
-				for (Object ob : obj)
-				{
-					if (ob instanceof Publication)
-					{
-						Publication p = (Publication) ob;
-						if (!pubfiltred.contains(p))
-						pubfiltred.add(p);
-				}
-				}
-			}
-	}
-
-		ArrayList<Publication> finalFiltredPub = new ArrayList<Publication>();
-	for (Map.Entry<Category, Integer> entry : memInt.getInterests().entrySet())
-		{
-		int i = 0;
-		for (Publication p : pubfiltred)
-			{
-				for (Category c : p.getCategory())
-				if (memInt.getInterests().containsKey(c))
-				{
-					if (!finalFiltredPub.contains(p))
-						finalFiltredPub.add(p);
-					i++;
-				}
-				if (i == entry.getValue())
-					break;
-			}
-		}
-		/*for (Map.Entry<Category, Integer> entry : memInt.getInterests().entrySet())
-		{
-			for (int i = 0; i < entry.getValue(); i++)
-			{
-
-			}
-		}
-
-		for (Publication p : finalFiltredPub)
-	{
-			log.info("p  : " + p.getTitle());
-				for (Category c : p.getCategory())
-				{
-					log.info("cat = " + c.getTitle());
-				}
-		}
-
-	model.addAttribute("listlasttenpub", tenLastPub);
-	model.addAttribute("finalFiltredPub", pubfiltred);
-	return "home";
-	}*/
-
-	//	@RequestMapping("/updatemember")
-	//	@ResponseBody
-	//	public String updateUser(@RequestParam(value="id", required=true) int id,
-	//			@RequestParam(value="email", required=true) String email,
-	//			@RequestParam(value="firstname", required=true) String firstname,
-	//			@RequestParam(value="lastname", required=true) String lastname) {
-	//		try {
-	//			Member user = datamem.findOne(id);
-	//			user.setEmail(email);
-	//			user.setFirstname(firstname);
-	//			user.setLastname(lastname);
-	//			datamem.save(user);
-	//		}
-	//		catch (Exception ex) {
-	//			return "Error updating the member: " + ex.toString();
-	//		}
-	//		return "member succesfully updated!";
-	//	}
 
 }
